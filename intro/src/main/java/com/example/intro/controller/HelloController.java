5289c6ebbb445ce8630c1b545a579a12ec89b95c
unknown <manojrawat87634@gmail.com>
2026-01-24 17:03:41 +0530

    inital comit

---
+++ intro/src/main/java/com/example/intro/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.intro.controller;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/api/first")
+    public Map<String, String> firstApi() {
+           return Map.of("data", "first api in Java");
+    }
+
+    @GetMapping("/message")
+    public Map<String, String> messageApi(){
+        return Map.of("name", "Manoj Rawat");
+    }
+
+   
+
+}
---
+++ intro/src/main/java/com/example/intro/controller/PostControler.java
@@ -0,0 +1,44 @@
+ package com.example.intro.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*") 
+public class PostControler {
+    @PostMapping("/api/first")
+    public String createUser(@RequestBody User user){
+        System.out.print(user);
+        return "";
+    }
+
+}
+
+
+class User {
+    private String name;
+    private String email;
+
+    public User() {} // default constructor
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters & Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', email='" + email + "'}";
+    }
+}
+
+ 
