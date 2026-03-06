134e340933b4dacacf0e8ec9b54007958ec6028e
unknown <manojrawat87634@gmail.com>
2026-03-06 17:29:05 +0530

    inital commit

---
+++ 05_spring_authentication_and_auth/04_JWT_Authentication/src/main/java/com/example/demo/models/UserModel.java
@@ -0,0 +1,58 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "email")
+})
+public class UserModel {
+        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UserModel() {
+    }
+
+    public UserModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ 05_spring_authentication_and_auth/04_JWT_Authentication/src/main/java/com/example/demo/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.demo.repo;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.UserModel;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<UserModel, Long> {
+  Optional<UserModel> findByEmail(String email);    
+}
